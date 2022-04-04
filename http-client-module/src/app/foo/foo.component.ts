45cd5fdb0bcb39bfb4e5dda12153d19709af7e35
Ramon-darwin <RAMON.DARWIN@iisgalvanimi.edu.it>
2022-04-04 08:30:07 +0000

    15.2

--- http-client-module/src/app/foo/foo.component.ts
+++ http-client-module/src/app/foo/foo.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
+import { Foo } from './foo.model';
 
 @Component({
  selector: 'app-foo',
@@ -8,10 +9,20 @@ import { Observable } from 'rxjs';
  styleUrls: ['./foo.component.css']
 })
 export class FooComponent {
+  fooData: Foo[] = [];
+  oFoo: Observable<Foo[]> | undefined;
+
    data : Object = !undefined;
    loading: boolean = false;
    o : Observable<Object> | undefined;
    constructor(public http: HttpClient) {}
+   makeTypedRequest() : void
+ {
+   //oFoo : Observable<Foo[]>; va dichiarato tra gli attributi della classe
+   this.oFoo = this.http.get<Foo[]>('https://jsonplaceholder.typicode.com/posts');
+   this.oFoo.subscribe(data => {this.fooData = data;});
+ }
+
    makeRequest(): void {
      console.log("here");
      this.loading = true;
@@ -51,6 +62,8 @@ export class FooComponent {
       this.data = data;
       this.loading = false;
     });
+  
+    
 }
   
 }
